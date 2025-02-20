1e1c7b7540fe888779ab5060af3acb3b3b279d7b
MetaBarj0 <troctsch.cpp@gmail.com>
2025-02-20 11:31:20 +0000

    feat: style and network

--- src/app/Todo.tsx
+++ src/app/Todo.tsx
@@ -1,19 +1,32 @@
 "use client";
 
-import { MouseEvent, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
 
-import { ethers } from "ethers";
+import { BrowserProvider, ethers, JsonRpcSigner } from "ethers";
+
+function formatAddress(address: string): string {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`
+}
 
 const Todo = () => {
-  const [buttonConnectText, setButtonConnectText] = useState(
-    "Connect your wallet",
-  );
-
+  const [buttonConnectText, setButtonConnectText] = useState("Connect your wallet");
   const [errorMessage, setErrorMessage] = useState("");
-  const [defaultAccount, setDefaultAccount] = useState(ethers.ZeroAddress);
-  const [signer, setSigner] = useState(undefined);
-  const [provider, setProvider] = useState(undefined);
+  const [defaultAccount, setDefaultAccount] = useState(formatAddress(ethers.ZeroAddress));
+  const [balance, setBalance] = useState("0 ETH");
+  const [signer, setSigner] = useState<JsonRpcSigner>();
+  const [provider, setProvider] = useState<BrowserProvider>();
   const [contract, setContract] = useState(undefined);
+
+  const updateBalance = () => {
+    if (!provider) return;
+    if (!signer) return;
+
+    provider.getNetwork()
+      .then(name => { console.log(name) })
+      .catch(error => setErrorMessage(error));
+  }
+
+  useEffect(updateBalance, [signer, provider]);
 
   const handleConnectButtonClick = async (_event: MouseEvent) => {
     if (!window.ethereum) {
@@ -24,17 +37,32 @@ const Todo = () => {
     await window.ethereum.request({ method: "eth_requestAccounts" });
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signers = await provider.listAccounts();
+    const signer = signers[0];
+    const address = await signers[0].getAddress();
 
-    setDefaultAccount(await signers[0].getAddress());
+    setDefaultAccount(formatAddress(address));
+    setProvider(provider);
+    setSigner(signer);
   };
 
   return (
     <div className="Todo">
-      <h3>Get/Set interaction with contract!</h3>
-      <button className="Button" onClick={handleConnectButtonClick}>
+      <h3 className="mb-4">Get/Set interaction with contract!</h3>
+      <button
+        className="bg-amber-500 rounded-md p-1 block"
+        onClick={handleConnectButtonClick}
+      >
         {buttonConnectText}
       </button>
-      <h1>{errorMessage}</h1>
+      <div className="grid grid-cols-2 gap-2 w-fit">
+        <div className="ReadOnlyField">
+          Connected account: {defaultAccount}
+        </div>
+        <div className="ReadOnlyField">Balance: {balance}</div>
+      </div>
+      <h1 className="text-red-500 block absolute bottom-0 left-0">
+        {errorMessage}
+      </h1>
     </div>
   );
 };
