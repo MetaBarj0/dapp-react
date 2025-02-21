88ba9580f70f81c4ddbb407ce0aeb102097a2f42
MetaBarj0 <troctsch.cpp@gmail.com>
2025-02-21 15:59:16 +0000

    refactor(WalletConnect): split
    separated layout from logic

--- src/app/WalletConnect/index.tsx
+++ src/app/WalletConnect/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { BrowserProvider, ethers } from "ethers";
-import { MouseEvent, useEffect, useState } from "react";
+import { BrowserProvider } from "ethers";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import useWalletConnect from "./useWalletConnect";
 
 type Props = {
-  setProvider: (provider: BrowserProvider) => void,
+  setProvider: Dispatch<SetStateAction<BrowserProvider | undefined>>,
   provider: BrowserProvider | undefined,
-  setErrorMessage: (error: string) => void
+  setErrorMessage: Dispatch<SetStateAction<string>>
 };
 
 const WalletConnect = ({ setProvider, provider, setErrorMessage }: Props) => {
@@ -14,61 +15,21 @@ const WalletConnect = ({ setProvider, provider, setErrorMessage }: Props) => {
   const [connectedAccount, setConnectedAccount] = useState("0x0000...0000");
   const [accountBalance, setAccountBalance] = useState("0 ETH");
 
-  function formatBalance(amount: bigint): string {
-    return `${ethers.formatEther(amount)}`;
-  }
+  const use = useWalletConnect({
+    provider, setProvider,
+    setErrorMessage,
+    connectedNetwork, setConnectedNetwork,
+    connectedAccount, setConnectedAccount,
+    accountBalance, setAccountBalance
+  });
 
-  function formatAddress(address: string): string {
-    return `${address.slice(0, 6)}...${address.slice(-4)}`
-  }
-
-  async function switchWalletNetworkToSepolia() {
-    if (!window.ethereum) {
-      setErrorMessage("You need to install a MetaMask compatible wallet");
-
-      return;
-    }
-
-    try {
-      await window.ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{
-          chainId: `0x${11155111n.toString(16)}`
-        }]
-      });
-
-      setProvider(new ethers.BrowserProvider(window.ethereum));
-    } catch (error) {
-      if (error instanceof Object && "message" in error && typeof error.message === "string")
-        setErrorMessage(error.message);
-    }
-  }
-
-  async function walletConnectHandler(_event: MouseEvent<HTMLButtonElement>) {
-    await switchWalletNetworkToSepolia();
-  }
-
-  async function afterProviderUpdate() {
-    if (!provider) return;
-
-    const network = await provider.getNetwork();
-    const signers = await provider.listAccounts();
-    const signer = signers[0];
-    const address = await signer.getAddress();
-    const balance = await provider.getBalance(signer);
-
-    setConnectedAccount(formatAddress(address));
-    setConnectedNetwork({ name: network.name, chainId: network.chainId });
-    setAccountBalance(formatBalance(balance));
-  }
-
-  useEffect(() => { afterProviderUpdate() }, [provider])
+  useEffect(() => { use.afterProviderUpdate() }, [provider])
 
   return (
     <div>
       <div className="border-solid border-2 rounded-md p-1 border-gray-500">
         <label className="mb-4 block w-auto text-center">Wallet connect</label>
-        <button className="bg-amber-500 rounded-md p-1 block w-full mb-4" onClick={walletConnectHandler}>Connect your wallet</button>
+        <button className="bg-amber-500 rounded-md p-1 block w-full mb-4" onClick={use.walletConnectHandler}>Connect your wallet</button>
         <label className="block">{`Connected network: ${connectedNetwork.name} (${connectedNetwork.chainId})`}</label>
         <label className="block">{`Connected account: ${connectedAccount}`}</label>
         <label className="block">{`Account balance: ${accountBalance}`}</label>
---
+++ src/app/WalletConnect/useWalletConnect.ts
@@ -0,0 +1,68 @@
+import { BrowserProvider, ethers } from "ethers";
+import { Dispatch, SetStateAction, MouseEvent } from "react";
+
+type Props = {
+  provider: BrowserProvider | undefined, setProvider: Dispatch<SetStateAction<BrowserProvider | undefined>>,
+  setErrorMessage: Dispatch<SetStateAction<string>>,
+  connectedNetwork: NetworkInfo, setConnectedNetwork: Dispatch<SetStateAction<NetworkInfo>>,
+  connectedAccount: string, setConnectedAccount: Dispatch<SetStateAction<string>>,
+  accountBalance: string, setAccountBalance: Dispatch<SetStateAction<string>>
+};
+
+export default function useWalletConnect(props: Props) {
+  return {
+    walletConnectHandler: async (_event: MouseEvent<HTMLButtonElement>) => {
+      await switchWalletNetworkToSepolia();
+    },
+
+    afterProviderUpdate: async () => {
+      if (!props.provider) return;
+
+      const network = await props.provider.getNetwork();
+      const signers = await props.provider.listAccounts();
+      const signer = signers[0];
+      const address = await signer.getAddress();
+      const balance = await props.provider.getBalance(signer);
+
+      props.setConnectedAccount(formatAddress(address));
+      props.setConnectedNetwork({ name: network.name, chainId: network.chainId });
+      props.setAccountBalance(formatBalance(balance));
+    }
+  };
+
+  async function switchWalletNetworkToSepolia() {
+    if (!window.ethereum) {
+      props.setErrorMessage("You need to install a MetaMask compatible wallet");
+
+      return;
+    }
+
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{
+          chainId: `0x${11155111n.toString(16)}`
+        }]
+      });
+
+      props.setProvider(new ethers.BrowserProvider(window.ethereum));
+    } catch (error) {
+      if (error instanceof Object && "message" in error && typeof error.message === "string")
+        props.setErrorMessage(error.message);
+    }
+  }
+
+  function formatBalance(amount: bigint): string {
+    return `${ethers.formatEther(amount)}`;
+  }
+
+  function formatAddress(address: string): string {
+    return `${address.slice(0, 6)}...${address.slice(-4)}`
+  }
+
+};
+
+type NetworkInfo = {
+  name: string,
+  chainId: bigint
+};
